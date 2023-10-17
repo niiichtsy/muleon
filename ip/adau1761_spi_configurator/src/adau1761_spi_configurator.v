1f83f940a62f78b8960c2055ed674e5d031505a4
neverducky <niiichtsy@gmail.com>
2023-10-17 19:03:31 +0200

    updated register write

--- ip/adau1761_spi_configurator/src/adau1761_spi_configurator.v
+++ ip/adau1761_spi_configurator/src/adau1761_spi_configurator.v
@@ -59,7 +59,7 @@ module adau1761_spi_configurator (
           cs <= 1'b0;
           spi_counter <= spi_counter + 1'b1;
           if (spi_counter == 8'h00) begin
-            sdo_reg <= {8'h00, 8'h40, 8'h00, 8'h01};  // Write a 0x1 to register 0x4000 to enable core clock
+            sdo_reg <= {8'h00, 8'h40, 8'h00, 8'h07};  // Write a 0x1 to register 0x4000 to enable core clock
             sdo <= 1'b0;  // NOTE: This bit is ignored - sdo coincidental with the cs falling edge is not read
           end else begin
             sdo_reg <= sdo_reg << 1;
