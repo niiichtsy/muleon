daab99e5fd693eeeaa04b922afc23af36167abea
niiichtsy <niiichtsy@gmail.com>
2023-11-26 14:51:27 +0100

    Updated SPI configurator testbench and tweaked register writes. Clocked on falling edge for testing

--- ip/adau1761_spi_configurator/src/adau1761_spi_configurator.v
+++ ip/adau1761_spi_configurator/src/adau1761_spi_configurator.v
@@ -4,7 +4,7 @@ module adau1761_spi_configurator (
     output reg cs,
     output reg [39:0] read_value,
     input [15:0] address,
-    input [15:0] write_value,
+    input [7:0] write_value,
     input write,
     input read,
     input init,
@@ -16,12 +16,11 @@ module adau1761_spi_configurator (
   // States
   localparam IDLE = 0;
   localparam INIT = 1;
-  localparam READ = 2;
-  localparam WRITE = 3;
+  localparam WRITE = 2;
+  localparam READ = 3;
 
   //Regs
-  reg [39:0] sdo_reg;
-  reg sdi_reg;
+  reg [31:0] sdo_reg;
   reg [7:0] spi_counter;
   reg [1:0] init_counter;
   reg [3:0] state;
@@ -31,9 +30,10 @@ module adau1761_spi_configurator (
   assign sclk = clk;
 
 
-  always @(posedge clk) begin
+  always @(negedge clk) begin
     if (!resetn) begin
       sdo <= 'h00;
+      sdo_reg <= 32'h00;
       cs <= 'b1;
       init_counter <= 'h00;
       spi_counter <= 'h00;
@@ -44,8 +44,14 @@ module adau1761_spi_configurator (
       case (state)
 
         IDLE: begin
-          if (write) state <= WRITE;
-          if (read) state <= READ;
+          if (write) begin
+            state   <= WRITE;
+            sdo_reg <= {8'h00, address, write_value};  // Write a 0x1 to register 0x4000 to enable core clock
+          end
+          if (read) begin
+            state   <= READ;
+            sdo_reg <= {8'h01, address, 8'h00};
+          end
           if (init) state <= INIT;
           transaction_done <= 1'b0;
         end
@@ -74,35 +80,25 @@ module adau1761_spi_configurator (
         WRITE: begin
           cs <= 1'b0;
           spi_counter <= spi_counter + 1'b1;
-          if (spi_counter == 8'h00) begin
-            sdo_reg <= {8'h00, address, write_value};  // Write a 0x1 to register 0x4000 to enable core clock
-            sdo <= 1'b0;  // NOTE: This bit is ignored - sdo coincidental with the cs falling edge is not read
-          end else begin
-            sdo_reg <= sdo_reg << 1;
-            sdo <= sdo_reg[23];  // MSB first
-          end
+          sdo <= sdo_reg[31];  // MSB first
+          sdo_reg <= sdo_reg << 1;
         end
 
         // Read check
         READ: begin
           cs <= 1'b0;
           spi_counter <= spi_counter + 1'b1;
-          if (spi_counter == 8'h00) begin
-            sdo_reg <= {8'h01, address, 16'h00};
-            sdo <= 1'b0;  // NOTE: This bit is ignored - sdo coincidental with the cs falling edge is not read
-          end else begin
-            sdo_reg <= sdo_reg << 1;
-            sdo <= sdo_reg[23];  // MSB first
-            if (spi_counter >= 'd24) begin
-              read_value[0] <= sdi;
-              read_value <= read_value << 1;
-            end
+          sdo <= sdo_reg[31];  // MSB first
+          sdo_reg <= sdo_reg << 1;
+          if (spi_counter >= 'd24) begin
+            read_value[0] <= sdi;
+            read_value <= read_value << 1;
           end
         end
 
       endcase
 
-      if (spi_counter == 8'h28) begin
+      if (spi_counter == 8'h20) begin
         cs <= 1'b1;
         spi_counter <= 8'h00;
         transaction_done <= 1'b1;
